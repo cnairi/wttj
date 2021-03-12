38b261831c65bde0ec47002ce6422495321042ee
Camille Nairi <cnairi@student.42.fr>
2021-03-12 11:47:06 +0100

    chore: continue tests implementation

---
+++ src/components/JobsList/__tests__/EmptyList.test.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import EmptyList from '@components/JobsList/EmptyList'
+import { render } from '@utils/tests'
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: str => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    }
+  },
+}))
+
+describe('Component Common <Language />', () => {
+  test('It should renders properly', () => {
+    const tree = render(<EmptyList />)
+    expect(tree).toMatchSnapshot()
+  })
+
+  test('It have title and text', async () => {
+    const { getByRole, getByText } = render(<EmptyList />)
+    const titleElement = getByRole('heading')
+    const textElement = getByText('filters.empty_list_title')
+    expect(titleElement).toHaveTextContent('filters.empty_list_title')
+    expect(textElement).toBeDefined()
+  })
+})
